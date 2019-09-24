0f9d5747961e1e7684b90bdbb7136ada7c187951
DDDDDYLAN5050 <55461461+DDDDDYLAN5050@users.noreply.github.com>
2019-09-24 18:16:13 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,30 @@
-function preload(){
-  // put preload code here
-}
-
+let myCanvas;
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  //background(20);
+  myCanvas = createCanvas(windowHeight,windowHeight);
+//createCanvas(windowHeight,windowHeight);
+  background(255);
   angleMode(DEGREES);
-  noFill();
-  colorMode(HSB);
-  // put setup code here
+  strokeWeight(0.1);
+  stroke(0);
+  noCursor();
+  m= width/3;
+  n= 0;
+  textSize(12);
+  textFont('Helvetica');
+  fill(0);
+  text('Drag to move', width/6, height/6);
+  text('Double click to redraw', width/6, height/6+15);
 }
 
 function draw() {
-
-  stroke(mouseX/800*255,mouseY/600*255,240);
-  fill(50);
+  translate(width/2,height/2);
+  myCanvas.position(-0.1*winMouseX, -0.1*winMouseY);
+  line(m,n,cos(frameCount*3)*width/3,sin(frameCount*3)*height/3);
   if (mouseIsPressed){
-    line(pmouseX,pmouseY,mouseX,mouseY);
-    }
+      m=mouseX-width/2;
+      n=mouseY-height/2;
+  }
+}
+function doubleClicked(){
+  background(255);
 }
